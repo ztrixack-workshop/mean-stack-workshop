e756fa398f523695bdb2990eaa03120f1f74035d
ATOM\Elena <ztrixack.th@gmail.com>
2016-11-03 04:26:24 +0700

    add jade module (view template)

--- config/express.js
+++ config/express.js
@@ -16,6 +16,9 @@ module.exports = function() {
 	}));
 	app.use(bodyParser.json());
 
+	app.set("views", "./app/views");
+	app.set("view engine", "jade");
+
 	require('../app/routes/index.routes')(app);
 	return app;
 }
