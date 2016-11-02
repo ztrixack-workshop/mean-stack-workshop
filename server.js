5f92e7eb4d4ed05722c33c70609b0e14a6090459
ATOM\Elena <ztrixack.th@gmail.com>
2016-11-03 02:30:15 +0700

    change to simple middleware connection

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-var http = require('http');
+var connect = require('connect');
+var app = connect();
 
-http.createServer(function(req, res) {
-	res.writeHead(200, {
-		'Content-Type': 'text/plain'
-	});
-	res.end('Hello World');
-}).listen(3000);
+// req -> HTTP request object
+// res -> HTTP response object
+// next -> next middleware function
+var logger = function(req, res, next) {
+	// middleware
+	console.log(req.method, req.url);
 
+	next();
+}
+
+var helloWorld = function(req, res, next) {
+	// middleware
+	res.setHeader('Content-Type', 'text/plain');
+	res.end("Hello World!");
+};
+
+app.use(logger);
+app.use(helloWorld);
+
+app.listen(3000);
 console.log('Server running at http://localhost:3000');
