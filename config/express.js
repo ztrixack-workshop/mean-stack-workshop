80b253e3553222556aefb0f2aba028f3baf59b11
ATOM\Elena <ztrixack.th@gmail.com>
2016-11-03 04:00:55 +0700

    add node env config

--- config/express.js
+++ config/express.js
@@ -1,7 +1,21 @@
 var express = require('express');
+var morgan = require('morgan');
+var compression = require('compression');
+var bodyParser = require('body-parser');
 
 module.exports = function() {
 	var app = express();
+
+	if (process.env.NODE_ENV === "development") {
+		app.use(morgan('dev'));
+	} else {
+		app.use(compression);
+	}
+	app.use(bodyParser.urlencoded({
+		extended: true
+	}));
+	app.use(bodyParser.json());
+
 	require('../app/routes/index.routes')(app);
 	return app;
 }
