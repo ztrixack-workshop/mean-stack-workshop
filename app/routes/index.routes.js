6b3a431da2c7ab2339c86c2a243b6ccda19c2449
ATOM\Elena <ztrixack.th@gmail.com>
2016-11-03 03:43:07 +0700

    mvc structure

---
+++ app/routes/index.routes.js
@@ -0,0 +1,9 @@
+module.exports = function(app) {
+	var index = require('../controllers/index.controller');
+	// app.get('/', index.render);
+	// app.post('/', index.render);
+	// app.put('/', index.render);
+	// app.delete('/', index.render);
+	// app.route('/').get(index.render).post(index.render);
+	app.all('/', index.render);
+}
--- server.js
+++ server.js
@@ -1,36 +1,7 @@
-var express = require('express');
+var express = require('./config/express');
 var app = express();
-
-// req -> HTTP request object
-// res -> HTTP response object
-// next -> next middleware function
-var logger = function(req, res, next) {
-	// middleware
-	console.log(req.method, req.url);
-
-	next();
-}
-
-var helloWorld = function(req, res, next) {
-	// middleware
-	// res.setHeader('Content-Type', 'text/plain');
-	// res.end("Hello World!");
-	// express can convert Content-Type automatically by send method
-	res.send("Hello World!");
-
-};
-
-var goodbyeWorld = function(req, res, next) {
-	res.send("Hello World!");	
-};
-
-// mounting request path
-app.use(logger);
-app.use('/hello', helloWorld);
-app.use('/goodbye', helloWorld);
-
 app.listen(3000);
-console.log('Server running at http://localhost:3000');
 
-// for other
 module.exports = app;
+
+console.log('Server runnung at http://localhost:3000');
