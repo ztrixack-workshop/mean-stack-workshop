875dde04449b43c047ce73da134b47fce70a8e3c
ATOM\Elena <ztrixack.th@gmail.com>
2016-11-03 12:53:37 +0700

    add sass view template

--- config/express.js
+++ config/express.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var compression = require('compression');
 var bodyParser = require('body-parser');
+var sass = require('node-sass-middleware');
 
 module.exports = function() {
 	var app = express();
@@ -16,11 +17,33 @@ module.exports = function() {
 	}));
 	app.use(bodyParser.json());
 
-	app.set("views", "./app/views");
-	app.set("view engine", "jade");
+	app.set('views', './app/views');
+	app.set('view engine', 'jade');
 
 	require('../app/routes/index.routes')(app);
 
+	// runtime must be below route for high perfomance
+	// before express.static
+	if (process.env.NODE_ENV === "development") {
+		app.use(sass({
+			src: './sass',
+			dest: './public/css',
+			outputStyle: 'compact', // compressed, compact, expandded
+			prefix: '/css',
+			indentedSyntax: true,
+			debug: true
+		}));
+	} else {
+		app.use(sass({
+			src: './sass',
+			dest: './public/css',
+			outputStyle: 'compressed',
+			indentedSyntax: true,
+			prefix: '/css'
+		}));
+	}
+
 	app.use(express.static('./public'));
+
 	return app;
 }
