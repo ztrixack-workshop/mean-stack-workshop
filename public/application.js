4526bd9bde53ea96efa39dd8fc5bcf2f3035c89c
ATOM\Elena <ztrixack.th@gmail.com>
2016-11-06 23:31:27 +0700

    simple 2-way data binding)

--- public/application.js
+++ public/application.js
@@ -3,6 +3,10 @@
 // angular.module(moduleName, [dependency1, dependency2, ...]);
 var mainAppModule = angular.module('app', []);
 
+mainAppModule.controller('NameController', ['$scope', function($scope) {
+	$scope.yourName = 'No Name';
+}]);
+
 // filter(filtername, function
 mainAppModule.filter('sayhello', function() {
 	return function (name) {
