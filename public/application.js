d18351b4c177f8671fc2705eb02286a4c0a3d67d
ATOM\Elena <ztrixack.th@gmail.com>
2016-11-07 00:00:47 +0700

    change bootstrap from Automatic to Manual

--- public/application.js
+++ public/application.js
@@ -1,8 +1,22 @@
 'use strict';
 
-// angular.module(moduleName, [dependency1, dependency2, ...]);
-var mainAppModule = angular.module('app', []);
+var mainAppModuleName = 'Main';
 
+// angular.module(moduleName, [dependency1, dependency2, ...]);
+var mainAppModule = angular.module(mainAppModuleName, []);
+
+angular.element(document).ready(function() {
+	angular.bootstrap(document.querySelector('#mainApp'), [mainAppModuleName], {
+		// use inline array annotation only!
+		strictDi: true
+	});
+});
+
+// inline array annotation:
+// 		( 'controllerName', ['$scope', '$http', function($scope, $http) {} ] )
+// implicit annotation: (minify/obfuscate problems, use ng-annotate to solve this!)
+// 		( 'controllerName', function($scope, $http) {} )
+// inject property annotation: Local variable, like as implicit (same problem)
 mainAppModule.controller('NameController', ['$scope', function($scope) {
 	$scope.yourName = 'No Name';
 }]);
